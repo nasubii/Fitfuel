5f4cc5fb3e5c404101e66bca64f70cd710924010
yuutasanui <otamajakushi0622@gmail.com>
2025-10-31 10:35:03 +0900

    ファイル作成
    CSSとPHPファイルだけ作成しました

---
+++ pages/G01_ホーム.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="G01_ホーム.css" rel="stylesheet" type="text/css">
+    <title>ホーム</title>
+</head>
+<body>
+    
+</body>
+</html>
