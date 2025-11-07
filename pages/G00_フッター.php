5b76dbd99a01d79a11099cc48241f10cb83b55d3
yuutasanui <otamajakushi0622@gmail.com>
2025-11-07 10:57:36 +0900

    cssのリンク修正

--- pages/G00_フッター.php
+++ pages/G00_フッター.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="G00_フッター.css" rel="stylesheet" type="text/css">
+    <link href="css/G00_フッター.css" rel="stylesheet" type="text/css">
     <title>フッター</title>
 </head>
 <body>
