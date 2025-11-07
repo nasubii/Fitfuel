fce45d7a1d41e1d8134876ffcc71f358e19c1a5e
2401016kota <2401016@s.asojuku.ac.jp>
2025-11-07 09:38:54 +0900

    ヘッダーの土台

--- pages/G00_ヘッダー.php
+++ pages/G00_ヘッダー.php
@@ -7,6 +7,37 @@
     <title>ヘッダー</title>
 </head>
 <body>
-    
+    <header class="site-header">
+        <div class="header-top">
+            <div class="container">
+                <div class="logo">
+                    <a href="../index.php" aria-label="FitFuel ホーム">
+                        <img src="../refpic/.svg" alt="FitFuel">
+                    </a>
+                </div>
+
+                <div class="search">
+                    <form action="../pages/G03_検索結果.php" method="get">
+                        <input type="text" name="q" placeholder="検索" aria-label="検索">
+                        <button type="submit">検索</button>
+                    </form>
+                </div>
+
+                <div class="icons">
+                    <a class="icon" href="#" title="マイページ">👤</a>
+                    <a class="icon" href="../pages/G04_カート一覧.php" title="カート">🛒</a>
+                    <a class="icon" href="../index.php" title="ホーム">🏠</a>
+                </div>
+            </div>
+        </div>
+
+        <nav class="category-nav">
+            <div class="container nav-inner">
+                <a href="#">ビタミン</a>
+                <a href="#">プロテイン</a>
+                <a href="#">サプリメント</a>
+            </div>
+        </nav>
+    </header>
 </body>
 </html>
