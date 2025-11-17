2119eb747db1abdf13d9b46bf4ca91e6eee5526f
newyusir <yusirou38@gmail.com>
2025-11-17 10:10:13 +0900

    purc

--- pages/G05_購入確認.php
+++ pages/G05_購入確認.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once '../config/database.php';
+require_once 'config/database.php';
 require_once __DIR__ . '/helpers/cart.php';
 
 if (!isset($_SESSION['customer'])) {
